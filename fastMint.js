912a7d7d7be758d75f1abdf73c2f315a3f733a06
nanqu <yingchuang.2324@gmail.com>
2023-12-08 09:54:48 +0700

    添加快速批量mint，不管结果如何累积nonce直接发送请求 代码逻辑优化

--- fastMint.js
+++ fastMint.js
@@ -9,34 +9,37 @@ const mintStr = process.env.MINT_STR;
 const utf8Bytes = Buffer.from(mintStr, 'utf-8');
 // 将字节转换为十六进制表示的字符串
 const hexStr = '0x' + utf8Bytes.toString('hex');
+const totalTimes = BigInt(process.env.NUMBER_OF_TIMES);
 
-// 每次交易都获取实时gas和nonce
+// 直到出错再获取gas和nonce，不管交易结果，一直发送交易
 async function performTransaction(walletInfo, numberOfTimes) {
+    const nonce = await web3.eth.getTransactionCount(walletInfo.address);
+    const gasPrice = await web3.eth.getGasPrice();
+    const gasEstimate = await web3.eth.estimateGas({
+        to: walletInfo.address,
+        value: amount,
+        data: hexStr,
+    });
+    let numberOfFinish = totalTimes - numberOfTimes;
     for (let i = 0; i < numberOfTimes; i++) {
+        numberOfFinish = numberOfFinish + BigInt(1);
         try {
-            const nonce = await web3.eth.getTransactionCount(walletInfo.address);
-            const gasPrice = await web3.eth.getGasPrice();
-            const gasEstimate = await web3.eth.estimateGas({
-                to: walletInfo.address,
-                value: amount,
-                data: hexStr,
-            });
-
             const transaction = {
                 to: walletInfo.address,
                 value: amount,
                 gasPrice: gasPrice,
-                nonce: nonce,
+                nonce: nonce + numberOfFinish - BigInt(1),
                 gas: gasEstimate,
                 data: hexStr,
             };
 
             const signedTransaction = await web3.eth.accounts.signTransaction(transaction, walletInfo.privateKey);
-            const result = await web3.eth.sendSignedTransaction(signedTransaction.rawTransaction);
+            web3.eth.sendSignedTransaction(signedTransaction.rawTransaction);
 
-            console.log(`第 ${walletInfo.num} 个地址 ${walletInfo.address} 第 ${i + 1} 次操作成功，交易哈希: ${result.transactionHash}`);
+            console.log(`第 ${walletInfo.num} 个地址 ${walletInfo.address} 第 ${numberOfFinish} 次操作成功.`);
         } catch (error) {
-            console.error(`第 ${walletInfo.num} 个地址 ${walletInfo.address} 第 ${i + 1} 次操作失败: `, error);
+            console.error(`第 ${walletInfo.num} 个地址 ${walletInfo.address} 第 ${numberOfFinish} 次操作失败: `, error);
+            await performTransaction(walletInfo, totalTimes - numberOfFinish);
         }
     }
 }
@@ -49,7 +52,7 @@ async function main() {
         console.log('未找到 evm_wallets.json 文件');
     }
 
-    Promise.all(walletData.map(wallet => performTransaction(wallet, process.env.NUMBER_OF_TIMES)))
+    Promise.all(walletData.map(wallet => performTransaction(wallet, totalTimes)))
         .then(() => {
             console.log("所有操作完成");
         })
